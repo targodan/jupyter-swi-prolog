b2755ddcadb6487926ca5c027ddcad05c6ea8daa
Luca Corbatto <luca@corbatto.de>
2018-10-28 18:00:28 +0100

    Adds dependencies and renames entrypoint

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jswip",
-    version="0.0.0",
+    version="0.0.1",
     author="Luca Corbatto",
     author_email="luca-pip@corbatto.de",
     description="A Jupyter Kernel for SWI-Prolog.",
@@ -13,12 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/targodan/jupyter-swi-prolog",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "pyswip",
+        "ipykernel"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     entry_points={
-        'console_scripts': ['jswipkernel=jswip.jupyter:main'],
+        'console_scripts': ['jswiplkernel=jswip.jupyter:main'],
     }
 )
